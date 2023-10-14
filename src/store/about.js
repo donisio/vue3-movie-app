d36f3971f9229e6503bfb3c89097b65a70021f2e
Donisio <donisio@naver.com>
2023-10-15 00:33:07 +0900

    about 추가

---
+++ src/store/about.js
@@ -0,0 +1,12 @@
+export default{
+    namespaced: true,
+    state:() =>{
+        return {
+            name : 'dionisio',
+            email: 'donisio@naver.com',
+            blog: 'N/A',
+            phone: '82+10-1234-5678',
+            image: 'https://heropy.blog/css/images/logo.png',
+        }
+    }
+}
